28ca69260e89064a26549593e3eca27d4179373d
Thomas Tsuchiya <tomt622@gmail.com>
2019-06-19 02:14:25 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const people = require('./people.json');
+
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Stuff and Things',
+        people: people.profiles
+    });
+});
+
+const server = app.listen(7000, () => {
+    console.log(`Express running → PORT ${server.address().port}`);
+});
+
+app.get('/profile', (req, res) => {
+    const person = people.profiles.find(p => p.id === req.query.id);
+    res.render('profile', {
+        title: `About ${person.firstname} ${person.lastname}`,
+        person,
+    });
+});
+
+app.get('/newpage', (req, res) => {
+    res.send('hi there');
+});
